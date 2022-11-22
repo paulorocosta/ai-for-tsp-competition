6a035d93bac1df3e83e14ab7cd09fe7df5cf656b
Robbert-Reijnen <robbert_reijnen@goodyear.com>
2022-11-22 13:09:15 +0100

    baseline alns

--- baseline_alns/env_alns.py
+++ baseline_alns/env_alns.py
@@ -4,6 +4,26 @@ from pathlib import Path
 import os
 
 
+def evaluate_individual_solution(instance_name, solution, n_nodes, seed):
+    base_path = Path(__file__).resolve().parents[1]
+    test_data_instance_path = base_path.joinpath('data/test/instances')
+    test_data_adj_path = base_path.joinpath('data/test/adjs')
+    x_path = os.path.join(test_data_instance_path, instance_name + '.csv')
+    adj_path = os.path.join(test_data_adj_path, 'adj-' + instance_name + '.csv')
+
+    env = Env(n_nodes=n_nodes, seed=seed, from_file=True, x_path=x_path, adj_path=adj_path)
+
+    total_reward, total_pen = 0, 0
+    for _ in range(5):
+        tour_time, rewards, pen, feas = env.check_solution(solution)
+        total_reward += rewards
+        total_pen += pen
+    score = - (total_reward + total_pen) / 5
+    if score == -0.0:
+        return 0
+    return score
+
+
 class alnsState:
 
     def __init__(self, nodes, init_tour, x, adj, instance_name, seed):
@@ -18,31 +38,12 @@ class alnsState:
     def copy(self):
         return copy.deepcopy(self)
 
-    def evaluate_individual_solution(self, instance_name, solution, n_nodes, seed):
-        base_path = Path(__file__).resolve().parents[1]
-        test_data_instance_path = base_path.joinpath('data/test/instances')
-        test_data_adj_path = base_path.joinpath('data/test/adjs')
-        x_path = os.path.join(test_data_instance_path, instance_name + '.csv')
-        adj_path = os.path.join(test_data_adj_path, 'adj-' + instance_name + '.csv')
-
-        env = Env(n_nodes=n_nodes, seed=seed, from_file=True, x_path=x_path, adj_path=adj_path)
-
-        total_reward, total_pen = 0, 0
-        for _ in range(5):
-            tour_time, rewards, pen, feas = env.check_solution(solution)
-            total_reward += rewards
-            total_pen += pen
-        score = - (total_reward + total_pen) / 5
-        if score == -0.0:
-            return 0
-        return score
-
     def objective(self):
         # all nodes must be present in the evaluation function: [1,2,3,1,5] --> will stop evaluation after second '1'
         tour = self.tour
         unvisited_nodes = [x for x in self.nodes if x not in tour]
         evaluation_tour = tour + unvisited_nodes
 
-        score = self.evaluate_individual_solution(self.instance_name, evaluation_tour, len(self.x), self.seed)
+        score = evaluate_individual_solution(self.instance_name, evaluation_tour, len(self.x), self.seed)
         return score
 
